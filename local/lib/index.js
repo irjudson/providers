b3224da17ab4e76558476d569b894e75c31dae4e
Tim Park <timfpark@gmail.com>
2014-10-24 19:42:37 -0700

    add ProxyMessageHub

--- local/lib/index.js
+++ local/lib/index.js
@@ -4,5 +4,6 @@ module.exports = {
     MemoryPubSubProvider:   require('./pubSub'),
     NullArchiveProvider:    require('./archive'),
     NullCacheProvider:      require('./nullCache'),
-    NullEmailProvider:      require('./email')
+    NullEmailProvider:      require('./email'),
+    ProxyMessageHub:        require('./messageHub')
 };
