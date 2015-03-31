d51a6d7ea6d5203d03b36f9890ce6c7d9f33511f
Tim Park <timfpark@gmail.com>
2015-03-31 16:11:30 -0700

    add Azure Queues pubsub provider

--- azure/lib/index.js
+++ azure/lib/index.js
@@ -1,6 +1,7 @@
 module.exports = {
-    AzureArchiveProvider:   require('./archive'),
-    AzureBlobProvider:      require('./blob'),
-    AzurePubSubProvider:    require('./pubSub'),
-    AzureEventHubProvider:  require('./eventhub')
+    AzureArchiveProvider:   		require('./archive'),
+    AzureBlobProvider:      		require('./blob'),
+    AzurePubSubProvider:    		require('./pubSub'),
+    AzureQueuesPubSubProvider:  	require('./queuesPubSub'),
+    AzureEventHubProvider:  		require('./eventhub')
 };
