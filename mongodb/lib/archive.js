34dc2e6153f0b2ab6691a933424686785ff43fd2
Ivan R. Judson <irjudson@gmail.com>
2015-09-13 14:27:21 -0700

    Fixing mongo update issues.

--- mongodb/lib/archive.js
+++ mongodb/lib/archive.js
@@ -1,5 +1,6 @@
-function MongoDBArchiveProvider(core) {
-    this.core = core;
+var core = require('nitrogen-core');
+
+function MongoDBArchiveProvider(config, log, callback) {
 }
 
 MongoDBArchiveProvider.prototype.archive = function(message, callback) {
@@ -14,17 +15,17 @@ MongoDBArchiveProvider.prototype.archive = function(message, callback) {
 };
 
 MongoDBArchiveProvider.prototype.find = function(principal, filter, options, callback) {
-    if (!this.core.models.Message.filterHasIndex(filter)) return callback(this.core.utils.badRequestError("filter: " + JSON.stringify(filter) + " does not have an index."));
-    var translatedFilter = this.core.utils.translateQuery(filter, this.core.models.Message.fieldTranslationSpec);
-    var filter = this.core.services.principals.filterForPrincipal(principal, translatedFilter);
+    if (!core.models.Message.filterHasIndex(filter)) return callback(core.utils.badRequestError("filter: " + JSON.stringify(filter) + " does not have an index."));
+    var translatedFilter = core.utils.translateQuery(filter, core.models.Message.fieldTranslationSpec);
+    var filter = core.services.principals.filterForPrincipal(principal, translatedFilter);
 
-    this.core.models.Message.find(filter, null, options, callback);
+    core.models.Message.find(filter, null, options, callback);
 };
 
 MongoDBArchiveProvider.prototype.remove = function(principal, filter, callback) {
-    if (!principal || !principal.is('service')) return callback(this.core.utils.authorizationError());
+    if (!principal || !principal.is('service')) return callback(core.utils.authorizationError());
 
-    this.core.models.Message.remove(filter, callback);
+    core.models.Message.remove(filter, callback);
 };
 
 module.exports = MongoDBArchiveProvider;
